b1f6a3ce328ed50826d8f79fe001059b63930c0b
Danil <koz.love.90@gmail.com>
2024-12-09 22:58:37 +0300

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,14 @@
+# # arguments_of _function
+#
+# def  send_email(message, recipient, * , sender = "university.help@gmail.com"):
+#
+#     print(f"'{message}', '{recipient}', '{sender}'")
+#
+#
+# send_email('пошел Нахер', 'pososi@mail.ru', sender= 'pup@gmail.com')
+
+a = 'koz.love.90@mail.ru'
+if 'k' in a:
+    print('есть')
+else:
+    print('нет')
