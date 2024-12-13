65796d7d60a452fa28740311889b1a284017a8b6
Danil <koz.love.90@gmail.com>
2024-12-13 12:53:36 +0300

    Рекурсия

--- recursion.py
+++ recursion.py
@@ -2,11 +2,13 @@
 def get_multiplied_digits(number):  # возвращает перемножение целых чисел
     str_number = str(int(number))  # если в строке пришли первые "0", то избавляемся от них
     first = int(str_number[0])  # берем первый элемент строки ка число
-    if len(str_number) <= 1:  # если в строке один элемент, то возвращаем его в first
+    if first == 0:
+        return 1
+    if len(str_number) == 1:  # если в строке один элемент, то возвращаем его в first
         return first
     return first * get_multiplied_digits(int(str_number[1:]))  # перемножаем first с результатом след вывода
                                                                #  get_multiplied_digits cо списком без first
 
 
-print(get_multiplied_digits(102030405))
-print(get_multiplied_digits('00102030405'))
+print(get_multiplied_digits(10203040))
+print(get_multiplied_digits('002030400'))
