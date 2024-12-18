4456e222f02cf945114969da56cd4a248aa5abdf
Danil <koz.love.90@gmail.com>
2024-12-18 22:00:52 +0300

    итоговое_модуля_3

--- module_3_summary.py
+++ module_3_summary.py
@@ -1,19 +1,20 @@
 # summary
 def calculate_structure_sum(*args):
-    global summa
-    for elem in args:
-        if isinstance(elem, (int, float)):
-            summa += int(elem)
-        elif isinstance(elem, str):
-            summa += len(elem)
-        elif isinstance(elem, (list, tuple, set)):
-            calculate_structure_sum(*elem)
-        elif isinstance(elem, dict):
-            calculate_structure_sum(*elem.items())
-    return summa
+    global result  # глобальная переменная, что бы каждый раз не обнулять при след. итерации
+    for elem in args:  # type(args) == tuple
+        if isinstance(elem, (int, float)):  # если элемент число
+            result += elem  # прибавляем к результату число
+        elif isinstance(elem, str):  # если элемент строка
+            result += len(elem)  # прибавляем к результату длину строки
+        elif isinstance(elem, (list, tuple, set)):  # если спископодобный* элемент
+            calculate_structure_sum(*elem)  # передаем в новую итерацию раскрытый список*
+        elif isinstance(elem, dict):  # если словарь
+            calculate_structure_sum(*elem.items())  # передаем распакованную пару ключ-значение
+
+    return result
 
 
-summa = 0
+result = 0
 data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
